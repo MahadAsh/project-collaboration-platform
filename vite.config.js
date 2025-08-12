39426aaa115eb3b054fb725552bd2d15eabe2809
MahadAsh <mahad8ash@gmail.com>
2025-08-13 01:43:00 +0500

    Make Vite base dynamic for Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/project-collaboration-platform/',
-  plugins: [react(),tailwindcss()],
+  base: process.env.NODE_ENV === 'production'
+    ? (process.env.VERCEL ? '/' : '/project-collaboration-platform/')
+    : '/',
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
